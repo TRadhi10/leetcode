c628f7ad8dfe89d2c5331c0890ada50d8c0f22c5
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-04 19:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (87.87%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+         while (left <= right) {
+            int mid = left + (right - left) / 2;
+             if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        int result = solution.searchInsert(nums, target);
+        System.out.println("Index where target " + target + " should be inserted: " + result);
+    }
+}
+
+   

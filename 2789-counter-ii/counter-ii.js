ad234f5126bb83ba436daed9293c936792832fff
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-07 22:29:13 +0530

    Time: 68 ms (13.44%) | Memory: 51.8 MB (37.18%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -1,14 +1,8 @@
 var createCounter = function(init) {
-    let temp=init;
-    return{
-       increment:function() {
-            return temp+=1;
-        },
-     reset:function() {
-            return temp=init;
-        },
-       decrement:function() {
-           return temp-=1;
-        }
-    }
+    let temp = init;
+    return {
+        increment: () => ++temp,
+        reset: () => temp = init,
+        decrement: () => --temp
+    };
 };

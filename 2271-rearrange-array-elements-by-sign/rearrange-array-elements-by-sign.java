16174bdd3d4329a2fa589aa3674683e9e847c7b5
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-16 20:31:20 +0530

    Time: 10 ms (16.17%) | Memory: 83 MB (17.59%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -14,7 +14,7 @@ class Solution {
         int[] result = new int[nums.length];
         int posIndex = 0, negIndex = 0, resultIndex = 0;
         
-        while (posIndex < positives.size() && negIndex < negatives.size()) {
+        while (posIndex < positives.size() && negIndex <negatives.size()) {
             result[resultIndex++] = positives.get(posIndex++);
             result[resultIndex++] = negatives.get(negIndex++);
         }

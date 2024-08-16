8eca7cc0d3541346fe50b66a9ae4b1204217718d
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-16 20:30:00 +0530

    Time: 10 ms (16.17%) | Memory: 83.6 MB (14.07%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,17 +1,24 @@
 class Solution {
-  public int[] rearrangeArray(int[] nums) {
-    int[] ans = new int[nums.length];
-    List<Integer> pos = new ArrayList<>();
-    List<Integer> neg = new ArrayList<>();
-
-    for (final int num : nums)
-      (num > 0 ? pos : neg).add(num);
-
-    for (int i = 0; i < pos.size(); ++i) {
-      ans[i * 2] = pos.get(i);
-      ans[i * 2 + 1] = neg.get(i);
+    public int[] rearrangeArray(int[] nums) {
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
+        
+        for (int num : nums) {
+            if (num > 0) {
+                positives.add(num);
+            } else {
+                negatives.add(num);
+            }
+        }
+        
+        int[] result = new int[nums.length];
+        int posIndex = 0, negIndex = 0, resultIndex = 0;
+        
+        while (posIndex < positives.size() && negIndex < negatives.size()) {
+            result[resultIndex++] = positives.get(posIndex++);
+            result[resultIndex++] = negatives.get(negIndex++);
+        }
+        
+        return result;
     }
-
-    return ans;
-  }
-}
+    }

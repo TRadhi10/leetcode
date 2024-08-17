8fbf4ca325bbe1d2a960dd35a220eebb1aecf38a
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-17 11:46:41 +0530

    Time: 12 ms (8.44%) | Memory: 44.8 MB (5.32%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -8,7 +8,7 @@ class Solution {
         int n = s.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
-                if ((s[j] + s[j + 1]).compareTo(s[j + 1] + s[j]) < 0) {
+                if ((s[j] + s[j + 1]).compareTo(s[j + 1] + s[j])<0) {
                     String temp = s[j];
                     s[j] = s[j + 1];
                     s[j + 1] = temp;

110dd6ad1242b576f2e065e54026604106250315
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-20 14:46:44 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (17.25%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,25 +1,24 @@
 class Solution {
     public int searchInsert(int[] nums, int target){
-        int left = 0;
-        int right = nums.length - 1;
-         while (left <= right) {
-            int mid = left + (right - left) / 2;
-             if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int temp=0;
+         for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+               temp=i;
             }
-        }
-        return left;
-    }
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {1, 3, 5, 6};
-        int target = 5;
-        int result = solution.searchInsert(nums, target);
-        System.out.println("Index where target " + target + " should be inserted: " + result);
+            else if(target<nums[0]){
+                   temp=0;
+            }
+            else if(nums[i]!=target){
+                if(nums[i]>target && nums[i-1]<target){
+                    temp=i;                
+                    }
+            }
+                 if(target>nums[nums.length-1])  {
+                    temp=nums.length;
+                 }
+         }
+         
+         return temp;
     }
 }
 

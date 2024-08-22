bac4304bfb519e69dd27c718339d0505628541a0
T Radhika <165305185+TRadhi10@users.noreply.github.com>
2024-08-22 16:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (67.03%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findComplement(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        int numberOfBits = (int) (Math.log(num) / Math.log(2)) + 1;
+        int bitmask = (1 << numberOfBits) - 1;
+        return num ^ bitmask;
+    }
+}
